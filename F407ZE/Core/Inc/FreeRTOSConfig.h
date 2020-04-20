50e2801f2c03b1a01be11a71140e5d9799e85311
John878953830 <878953830@qq.com>
2020-04-20 17:50:59 +0800

    modify can not stop broadcast when 100 ms period

--- F407ZE/Core/Inc/FreeRTOSConfig.h
+++ F407ZE/Core/Inc/FreeRTOSConfig.h
@@ -93,7 +93,7 @@ extern uint32_t ulHighFrequencyTimerTicks;
 
 /* Software timer definitions. */
 #define configUSE_TIMERS                         1
-#define configTIMER_TASK_PRIORITY                ( 2 )
+#define configTIMER_TASK_PRIORITY                ( 24 )
 #define configTIMER_QUEUE_LENGTH                 10
 #define configTIMER_TASK_STACK_DEPTH             256
 
